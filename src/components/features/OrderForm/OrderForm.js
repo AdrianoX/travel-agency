4a5a851fe466331f405f2fc64e6ecca69dd61000
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-25 09:40:27 +0200

    Add new files for next part of project

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,19 @@
+import React from  'react';
+// import PropTypes from 'prop-types';
+import {Row, Col} from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+
+const OrderForm = () => (
+
+
+  <Row>
+    <Col xs={12}>
+      <OrderSummary  />
+    </Col>
+  </Row>
+
+
+);
+
+
+export default OrderForm;
