f9f2f7c309ee2f2cf668bdc1c99b6217759aea3e
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-25 19:42:15 +0200

    Add OrderForm and OrderSummary components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,6 +1,6 @@
 import React from  'react';
 // import PropTypes from 'prop-types';
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
 const OrderForm = () => (
@@ -13,7 +13,37 @@ const OrderForm = () => (
   </Row>
 
 
+
+
 );
 
 
 export default OrderForm;
+
+
+
+
+
+// import React from  'react';
+// import PropTypes from 'prop-types';
+// import { Row, Col} from 'react-flexbox-grid';
+// import OrderSummary from '../OrderSummary/OrderSummary';
+
+// const OrderForm = props => (
+
+//                 <=== with/without <grid> ?
+//   <Row>
+//     <Col xs={12}>
+//       <OrderSummary tripCost={props.tripCost} />
+//     </Col>
+//   </Row>
+
+
+// );
+
+// OrderForm.propTypes = {
+//     cost: PropTypes.string,
+// }
+
+// export default OrderForm;
+
---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './OrderSummary.scss';
+
+
+const OrderSummary = () => {
+
+  <h2 className={styles.component}>Total: <strong>$12,345</strong></h2>;
+
+};
+
+
+export default OrderSummary;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import styles from './OrderSummary.scss';
+// import PropTypes from 'prop-types';
+
+
+// const OrderSummary = props => (
+
+//     <Grid>
+//       <Row>
+//         <Col xs={12}>
+//          <h2 className={styles.component}>Total: <strong>$12,345</strong></h2>;
+//         </Col>
+//       </Row>
+//     </Grid>
+//   );
+
+// OrderSummary.propTypes = {
+
+// };
+
+
+// export default OrderSummary;
+
