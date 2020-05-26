5ad036755c6aea4b240a93d6debc1d24f2b3937d
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-26 10:37:35 +0200

    [SOLVED] Problem with display Trip options and total price

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './OrderSummary.scss';
 
 
-const OrderSummary = () => {
+const OrderSummary = () => 
 
   <h2 className={styles.component}>Total: <strong>$12,345</strong></h2>;
 
-};
+
 
 
 export default OrderSummary;
