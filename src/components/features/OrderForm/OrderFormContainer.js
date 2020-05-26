6c9088504ba3b5c75643ac2bb905cf0efa328106
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-26 13:03:18 +0200

    [SOLVED] Small probles with the correct price display

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,17 +1,34 @@
-
-
-
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import OrderForm from './OrderForm';
 import {getOrderOptions} from '../../../redux/orderRedux';
 
 
-const mapStateToProps = props =>({            //<--- check ?
-  options: getOrderOptions (props),
+
+const mapStateToProps = state => ({
+  options: getOrderOptions(state),
 
 });
 
+
+
 export default connect(mapStateToProps)(OrderForm);
+
+
+
+
+
+
+// import {connect} from 'react-redux';
+// import OrderForm from './OrderForm';
+// import {getOrderOptions} from '../../../redux/orderRedux';
+
+
+// const mapStateToProps = props =>({            <--- check ?
+//   options: getOrderOptions (props),
+
+// });
+
+// export default connect(mapStateToProps)(OrderForm);
 
 
 
