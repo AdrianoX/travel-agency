03b5c966f424b785749ea0d16188e5308ddeab86
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-26 18:05:24 +0200

    [SOLVED?] Problem with options titles into trip

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
 
 
-const OrderOption = () => 
+const OrderOption = ({name}) => 
 
   <div className={styles.component}>
     <h3 className={styles.title}>{name} </h3>
   </div>;
 
-
-
-
-
-
+OrderOption.propTypes = {
+  name: PropTypes.string,
+};
 
 
 export default OrderOption;
+
+
+
