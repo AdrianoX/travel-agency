70464dd2300307433f7099017592e0f2e0e83e97
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-26 17:11:03 +0200

    Problem with options titles into trip

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
-
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 
 const OrderForm = props => (
 
   <Grid>
     <Row>
+      {pricing.map(option =>
+        <Col key={option.id} md={4}>
+          <OrderOption {...option}/>
+        </Col>
+      )}
       <Col xs={12}>
         <OrderSummary tripCost={props.tripCost} tripOptions={props.options}/>
       </Col>
