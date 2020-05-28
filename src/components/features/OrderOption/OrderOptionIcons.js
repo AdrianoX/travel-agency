bed28d30cca9c9057820dafd2f1dfe4c3a905387
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-28 09:36:03 +0200

    Add CarRental(dropdown), work on Accomodation(icons)

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,17 +1,47 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+
+const OrderOptionIcons = ({values, required}) => (
+  <div
+    className={styles.icon}
+  >
+    {required ? '' : (
+      <div key='null' value='' className={styles.icon}>---</div>
+    )}
+    {values.map(value => (
+      <div key={value.id} value={value.id}>{value.name} ({formatPrice(value.price)})</div>
+    ))}
+  </div>
+);
 
 
-const OrderOptionIcons =()=> {
 
-  return(
-    <div >OrderOptionIcons</div>
-  );
+
+
+OrderOptionIcons.propTypes = {
+  //   name: PropTypes.string,
+  values: PropTypes.array,
+  required: PropTypes.string,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
 };
+
 
 export default OrderOptionIcons;
 
 
+
+
+
+
+// const OrderOptionIcons =()=> {
+
+//     return(
+//       <div >OrderOptionIcons</div>
+//     );
+//   };
 
 // import React from 'react';
 
