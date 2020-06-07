bdbfa3bc5e24a334bc8fe72ad06fc83838653bc7
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-06-07 11:00:42 +0200

    Add formatTime function (hh:mm:ss)

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,20 +1,29 @@
 // export const formatTime = () => 'formatted time';
 
+// export const formatTime = ( time ) => {
+//   if (time == null ) {   // without arg
+//     return null;
+//   } else if (isNaN(time)) {   // if NaN
+//     return null; 
+//   } else if ( time < 0) {   // number less than 0
+//     return null;
+//   } else {
+
 export const formatTime = ( time ) => {
-  if (time == null ) {   // without arg
+  if (time == null || isNaN(time) || time < 0 ) {
     return null;
-  } else if (isNaN(time)) {   // if NaN
-    return null; 
-  } else if ( time < 0) {   // number less than 0
-    return null;
-  } else {
-
-
-    let second = Math.floor(time % 60);       //?
-    let minute = Math.floor((time / 60) % 60);  //?
-    let hour = Math.floor(time / 3600);       //?
-
-    return hour, minute, second;              //  ? 
   }
 
+
+  let seconds = Math.floor(time % 60) + '';   
+  seconds = seconds.padStart(2, '0');
+
+  let minutes = Math.floor((time / 60) % 60) + '';
+  minutes = minutes.padStart(2, '0');
+
+  let hours = Math.floor(time / 3600) + '';     
+  hours = hours.padStart(2, '0');  
+
+  return hours + ':' + minutes + ':' + seconds;              
 };
+
