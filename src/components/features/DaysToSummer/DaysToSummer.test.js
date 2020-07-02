e3c337336727e6441805dc49d0ca53bb11de4bca
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-07-02 21:55:01 +0200

    [UPDATE vs2] DaysToSummer/test

--- src/components/features/DaysToSummer/DaysToSummer.test.js
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -2,18 +2,12 @@ import React from 'react';
 import DaysToSummer from './DaysToSummer';
 import { shallow } from 'enzyme';
 
-
-// const select = {
-//   title: '.title',
-//   promoDescription: '.promoDescription',
-// };
-
 const mockProps = {
-  title: 'Count days to summer', // 34PR
+  title: 'Count days to summer',
 };
 
 const trueDate = Date;
-const mockDate = customDate => class extends Date {   // HappyHourAd.test / Test2
+const mockDate = customDate => class extends Date {
   constructor(...args) {
     if (args.length) {
       super(...args);
@@ -27,7 +21,7 @@ const mockDate = customDate => class extends Date {   // HappyHourAd.test / Test
   }
 };
 
-describe('Componbentt DaysToSummer', () => {
+describe('Component DaysToSummer', () => {
   it('should render correct without any crashing', () => {
     const component = shallow(<DaysToSummer />);
     expect(component).toBeTruthy();
@@ -36,7 +30,7 @@ describe('Componbentt DaysToSummer', () => {
 
 const checkDescriptionAtDate = (date, expectedDescription) => {    //
   it(`should show correct at ${date}`, () => {
-    global.Date = mockDate(`${date}T00:00:00.000Z`);    // change global time for date / 000Z
+    global.Date = mockDate(`${date}T00:00:00.000Z`);
 
     const component = shallow(<DaysToSummer {...mockProps}/>);
     const renderedDays = component.find('.title').text();
@@ -52,36 +46,4 @@ describe('Component DaysToSummer with mockDate(000Z)', () => {
   checkDescriptionAtDate('2019-11-30', '204 days to summer : ]');
   checkDescriptionAtDate('2019-10-29', '236 days to summer : ]');
 });
-
-// import React from 'react';
-// import {shallow} from 'enzyme';
-// import DaysToSummer from './DaysToSummer';
-
-
-// const select = {
-//   title: '.title',
-// };
-
-// const mockProps = {
-//   days: 13,
-// };
-
-// describe('Component DaysToSummer', () =>{
-//   it('Should render component without crashing', () => {
-//     const component = shallow(<DaysToSummer />);
-//     expect(component).toBeTruthy();
-//   });
-
-//   it('should render title', () => {
-//     const component = shallow(<DaysToSummer />);
-//     expect(component.exists(select.title)).toEqual(true);
-//   });
-
-//   it('should recive title from mockProps', () => {
-//     const component = shallow(<DaysToSummer {...mockProps} />);
-//     expect(component.find(select.title).text()).toEqual('13 days to summer !');
-//   });
-
-
-// });
 
